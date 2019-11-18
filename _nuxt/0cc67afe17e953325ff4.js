a555160af515c34da5c2f4bfbe64d87acbbcacea
Ta <ao.tong@adachina.com>
2019-11-18 13:25:45 +0800

    ???

--- _nuxt/0cc67afe17e953325ff4.js
+++ _nuxt/0cc67afe17e953325ff4.js
@@ -1,1 +1,122 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{158:function(t,d,o){var content=o(164);"string"==typeof content&&(content=[[t.i,content,""]]),content.locals&&(t.exports=content.locals);(0,o(20).default)("0bcbb242",content,!0,{sourceMap:!1})},163:function(t,d,o){"use strict";var n=o(158);o.n(n).a},164:function(t,d,o){(t.exports=o(19)(!1)).push([t.i,".about[data-v-7bddde1f]{padding:20px 0}.about .title[data-v-7bddde1f]{font-size:60px;text-align:center;font-weight:900;margin-bottom:20px}.about .isTablet[data-v-7bddde1f]{display:none}.about .introduction[data-v-7bddde1f]{color:#969696;font-size:18px}.about .gallery[data-v-7bddde1f]{margin-top:20px}.about .gallery img[data-v-7bddde1f]{width:100%}.about .tcolor[data-v-7bddde1f]{color:#c7a063;margin-top:40px}.about .dk[data-v-7bddde1f],.about .tcolor[data-v-7bddde1f]{margin-bottom:40px}.about .dk img[data-v-7bddde1f]{width:100%}.about .dk-info[data-v-7bddde1f]{color:#969696;font-size:18px;text-align:center;padding-bottom:10px}@media (max-width:780px){.about .title[data-v-7bddde1f]{font-size:40px}.about .isDesktop[data-v-7bddde1f]{display:none}.about .isTablet[data-v-7bddde1f]{display:block}.about .dk-info[data-v-7bddde1f],.about .introduction[data-v-7bddde1f]{font-size:16px}}@media (max-width:320px){.about .title[data-v-7bddde1f]{font-size:25px}}",""])},174:function(t,d,o){"use strict";o.r(d);var n={layout:"layout"},e=(o(163),o(8)),component=Object(e.a)(n,(function(){var t=this.$createElement;this._self._c;return this._m(0)}),[function(){var t=this,d=t.$createElement,o=t._self._c||d;return o("div",{staticClass:"about"},[o("div",{staticClass:"container"},[o("h2",{staticClass:"title isDesktop"},[t._v("信仰专业的力量 品牌源于专业")]),t._v(" "),o("h2",{staticClass:"title isTablet"},[t._v("信仰专业的力量"),o("br"),t._v("品牌源于专业")]),t._v(" "),o("p",{staticClass:"introduction"},[t._v("\n      DKbrand拥有从策略到设计、再到制作落地的完整产业链，我们的核心服务是通过科学有效的、国际化的设计和品牌管理之道，帮助客户构建卓越的品牌体系，协助更多的中国企业塑造国际化的品牌形象，在世界范围内树立属于中国企业的优秀的品牌形象和人文风格。\n      我们信仰专业的力量，用优秀的设计帮助客户降低企业运营成本，创造超越竞争对手的品牌竞争力，以深度拓展设计的商业价值，用品牌的力量使客户实现商业目标，用品牌的力量驱动企业的持续发展。\n    ")]),t._v(" "),o("div",{staticClass:"gallery"},[o("img",{attrs:{src:"/images/aboutus/gallery.png",alt:""}})]),t._v(" "),o("h2",{staticClass:"title tcolor"},[t._v("全产业链360°品牌服务")]),t._v(" "),o("div",{staticClass:"dk"},[o("img",{attrs:{src:"/images/aboutus/dk.png",alt:""}})]),t._v(" "),o("p",{staticClass:"dk-info"},[t._v("DKbrand集结了中国颇具实力与创新力的有为之士")]),t._v(" "),o("p",{staticClass:"dk-info"},[t._v("\n      拥有一个汇聚国内外知名专家学者教授及资深行业经验的视觉设计和品牌咨询精英组成的优秀团队,\n    ")]),t._v(" "),o("p",{staticClass:"dk-info"},[t._v("\n      有效的确保我们的设计和品牌管理理念与国际领先水平保持同步。\n    ")]),t._v(" "),o("p",{staticClass:"dk-info"},[t._v("\n      DKbrand拥有国际联盟合作伙伴组成的全球跨领域合作网络，\n    ")]),t._v(" "),o("p",{staticClass:"dk-info"},[t._v("\n      全面覆盖从品牌研究、视觉设计、产品造型到空间规划的顶尖力量，\n    ")]),t._v(" "),o("p",{staticClass:"dk-info"},[t._v("强强联手，为客户创造世界领先的品牌价值。")]),t._v(" "),o("p",{staticClass:"dk-info"},[t._v("\n      同时，欢迎热爱设计的朋友们加盟DKbrand，共同实现人生价值，为中国设计的发展作出我们的贡献。\n    ")])])])}],!1,null,"7bddde1f",null);d.default=component.exports}}]);
+;(window.webpackJsonp = window.webpackJsonp || []).push([
+  [2],
+  {
+    158: function(t, d, o) {
+      var content = o(164)
+      'string' == typeof content && (content = [[t.i, content, '']]),
+        content.locals && (t.exports = content.locals)
+      ;(0, o(20).default)('0bcbb242', content, !0, { sourceMap: !1 })
+    },
+    163: function(t, d, o) {
+      'use strict'
+      var n = o(158)
+      o.n(n).a
+    },
+    164: function(t, d, o) {
+      ;(t.exports = o(19)(!1)).push([
+        t.i,
+        '.about[data-v-7bddde1f]{padding:20px 0}.about .title[data-v-7bddde1f]{font-size:60px;text-align:center;font-weight:900;margin-bottom:20px}.about .isTablet[data-v-7bddde1f]{display:none}.about .introduction[data-v-7bddde1f]{color:#969696;font-size:18px}.about .gallery[data-v-7bddde1f]{margin-top:20px}.about .gallery img[data-v-7bddde1f]{width:100%}.about .tcolor[data-v-7bddde1f]{color:#c7a063;margin-top:40px}.about .dk[data-v-7bddde1f],.about .tcolor[data-v-7bddde1f]{margin-bottom:40px}.about .dk img[data-v-7bddde1f]{width:100%}.about .dk-info[data-v-7bddde1f]{color:#969696;font-size:18px;text-align:center;padding-bottom:10px}@media (max-width:780px){.about .title[data-v-7bddde1f]{font-size:40px}.about .isDesktop[data-v-7bddde1f]{display:none}.about .isTablet[data-v-7bddde1f]{display:block}.about .dk-info[data-v-7bddde1f],.about .introduction[data-v-7bddde1f]{font-size:16px}}@media (max-width:320px){.about .title[data-v-7bddde1f]{font-size:25px}}',
+        ''
+      ])
+    },
+    174: function(t, d, o) {
+      'use strict'
+      o.r(d)
+      var n = { layout: 'layout' },
+        e = (o(163), o(8)),
+        component = Object(e.a)(
+          n,
+          function() {
+            var t = this.$createElement
+            this._self._c
+            return this._m(0)
+          },
+          [
+            function() {
+              var t = this,
+                d = t.$createElement,
+                o = t._self._c || d
+              return o('div', { staticClass: 'about' }, [
+                o('div', { staticClass: 'container' }, [
+                  o('h2', { staticClass: 'title isDesktop' }, [
+                    t._v('信仰专业的力量 品牌源于专业')
+                  ]),
+                  t._v(' '),
+                  o('h2', { staticClass: 'title isTablet' }, [
+                    t._v('信仰专业的力量'),
+                    o('br'),
+                    t._v('品牌源于专业')
+                  ]),
+                  t._v(' '),
+                  o('p', { staticClass: 'introduction' }, [
+                    t._v(
+                      '\n      DKbrand拥有从策略到设计、再到制作落地的完整产业链，我们的核心服务是通过科学有效的、国际化的设计和品牌管理之道，帮助客户构建卓越的品牌体系，协助更多的中国企业塑造国际化的品牌形象，在世界范围内树立属于中国企业的优秀的品牌形象和人文风格。\n      我们信仰专业的力量，用优秀的设计帮助客户降低企业运营成本，创造超越竞争对手的品牌竞争力，以深度拓展设计的商业价值，用品牌的力量使客户实现商业目标，用品牌的力量驱动企业的持续发展。\n    '
+                    )
+                  ]),
+                  t._v(' '),
+                  o('div', { staticClass: 'gallery' }, [
+                    o('img', {
+                      attrs: { src: '/images/aboutus/gallery.png', alt: '' }
+                    })
+                  ]),
+                  t._v(' '),
+                  o('h2', { staticClass: 'title tcolor' }, [
+                    t._v('全产业链360°品牌服务')
+                  ]),
+                  t._v(' '),
+                  o('div', { staticClass: 'dk' }, [
+                    o('img', {
+                      attrs: { src: '/images/aboutus/dk.png', alt: '' }
+                    })
+                  ]),
+                  t._v(' '),
+                  o('p', { staticClass: 'dk-info' }, [
+                    t._v('DKbrand集结了中国颇具实力与创新力的有为之士')
+                  ]),
+                  t._v(' '),
+                  o('p', { staticClass: 'dk-info' }, [
+                    t._v(
+                      '\n      拥有一个汇聚国内外知名专家学者教授及资深行业经验的视觉设计和品牌咨询精英组成的优秀团队,\n    '
+                    )
+                  ]),
+                  t._v(' '),
+                  o('p', { staticClass: 'dk-info' }, [
+                    t._v(
+                      '\n      有效的确保我们的设计和品牌管理理念与国际领先水平保持同步。\n    '
+                    )
+                  ]),
+                  t._v(' '),
+                  o('p', { staticClass: 'dk-info' }, [
+                    t._v(
+                      '\n      DKbrand拥有国际联盟合作伙伴组成的全球跨领域合作网络，\n    '
+                    )
+                  ]),
+                  t._v(' '),
+                  o('p', { staticClass: 'dk-info' }, [
+                    t._v(
+                      '\n      全面覆盖从品牌研究、视觉设计、产品造型到空间规划的顶尖力量，\n    '
+                    )
+                  ]),
+                  t._v(' '),
+                  o('p', { staticClass: 'dk-info' }, [
+                    t._v('强强联手，为客户创造世界领先的品牌价值。')
+                  ]),
+                  t._v(' '),
+                  o('p', { staticClass: 'dk-info' }, [
+                    t._v(
+                      '\n      同时，欢迎热爱设计的朋友们加盟DKbrand，共同实现人生价值，为中国设计的发展作出我们的贡献。\n    '
+                    )
+                  ])
+                ])
+              ])
+            }
+          ],
+          !1,
+          null,
+          '7bddde1f',
+          null
+        )
+      d.default = component.exports
+    }
+  }
+])
--- _nuxt/151cb7d53ecdd1ed6195.js
+++ _nuxt/151cb7d53ecdd1ed6195.js
@@ -1,1 +1,118 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{162:function(t,e,r){var content=r(173);"string"==typeof content&&(content=[[t.i,content,""]]),content.locals&&(t.exports=content.locals);(0,r(20).default)("139e72c6",content,!0,{sourceMap:!1})},172:function(t,e,r){"use strict";var n=r(162);r.n(n).a},173:function(t,e,r){(t.exports=r(19)(!1)).push([t.i,".brand[data-v-210453ba]{display:-webkit-box;display:flex;flex-wrap:wrap;-webkit-box-pack:justify;justify-content:space-between;margin:20px 0 10px}.brand-item[data-v-210453ba]{overflow:hidden;cursor:pointer;margin-bottom:10px;flex-basis:49.6%}.brand-item .img[data-v-210453ba]{width:100%;height:100%;-webkit-transition:all .35s ease-in-out;transition:all .35s ease-in-out;-webkit-transform:scale(1);transform:scale(1)}.brand-item .img img[data-v-210453ba]{width:100%}.brand-item .img[data-v-210453ba]:hover{-webkit-transform:scale(1.1);transform:scale(1.1)}.story[data-v-210453ba]{display:-webkit-box;display:flex;flex-wrap:wrap;-webkit-box-pack:justify;justify-content:space-between;margin-bottom:10px}.story-item[data-v-210453ba]{overflow:hidden;cursor:pointer;margin-bottom:5px;flex-basis:16%}.story-item .img[data-v-210453ba]{width:100%;height:100%;-webkit-transition:all .35s ease-in-out;transition:all .35s ease-in-out;-webkit-transform:scale(1);transform:scale(1)}.story-item .img img[data-v-210453ba]{width:100%}.story-item .img[data-v-210453ba]:hover{-webkit-transform:scale(1.1);transform:scale(1.1)}@media (max-width:779px){.story-item[data-v-210453ba]{flex-basis:24.5%}}",""])},178:function(t,e,r){"use strict";r.r(e);var n={name:"Home",layout:"layout"},d=(r(172),r(8)),component=Object(d.a)(n,(function(){var t=this.$createElement,e=this._self._c||t;return e("div",{staticClass:"main"},[e("div",{staticClass:"container"},[this._m(0),this._v(" "),e("div",{staticClass:"story"},this._l(36,(function(t){return e("div",{key:t,staticClass:"story-item"},[e("div",{staticClass:"img"},[e("div",{staticClass:"img"},[e("img",{attrs:{src:"/images/index/a"+t+".png"}})])])])})),0)])])}),[function(){var t=this.$createElement,e=this._self._c||t;return e("div",{staticClass:"brand"},[e("div",{staticClass:"brand-item"},[e("div",{staticClass:"img"},[e("img",{attrs:{src:"/images/index/brand1.png",alt:""}})])]),this._v(" "),e("div",{staticClass:"brand-item"},[e("div",{staticClass:"img"},[e("img",{attrs:{src:"/images/index/brand2.png",alt:""}})])]),this._v(" "),e("div",{staticClass:"brand-item"},[e("div",{staticClass:"img"},[e("img",{attrs:{src:"/images/index/brand3.png",alt:""}})])]),this._v(" "),e("div",{staticClass:"brand-item"},[e("div",{staticClass:"img"},[e("img",{attrs:{src:"/images/index/brand4.png",alt:""}})])]),this._v(" "),e("div",{staticClass:"brand-item"},[e("div",{staticClass:"img"},[e("img",{attrs:{src:"/images/index/brand5.png",alt:""}})])]),this._v(" "),e("div",{staticClass:"brand-item"},[e("div",{staticClass:"img"},[e("img",{attrs:{src:"/images/index/brand6.png",alt:""}})])])])}],!1,null,"210453ba",null);e.default=component.exports}}]);
+;(window.webpackJsonp = window.webpackJsonp || []).push([
+  [5],
+  {
+    162: function(t, e, r) {
+      var content = r(173)
+      'string' == typeof content && (content = [[t.i, content, '']]),
+        content.locals && (t.exports = content.locals)
+      ;(0, r(20).default)('139e72c6', content, !0, { sourceMap: !1 })
+    },
+    172: function(t, e, r) {
+      'use strict'
+      var n = r(162)
+      r.n(n).a
+    },
+    173: function(t, e, r) {
+      ;(t.exports = r(19)(!1)).push([
+        t.i,
+        '.brand[data-v-210453ba]{display:-webkit-box;display:flex;flex-wrap:wrap;-webkit-box-pack:justify;justify-content:space-between;margin:20px 0 10px}.brand-item[data-v-210453ba]{overflow:hidden;cursor:pointer;margin-bottom:10px;flex-basis:49.6%}.brand-item .img[data-v-210453ba]{width:100%;height:100%;-webkit-transition:all .35s ease-in-out;transition:all .35s ease-in-out;-webkit-transform:scale(1);transform:scale(1)}.brand-item .img img[data-v-210453ba]{width:100%}.brand-item .img[data-v-210453ba]:hover{-webkit-transform:scale(1.1);transform:scale(1.1)}.story[data-v-210453ba]{display:-webkit-box;display:flex;flex-wrap:wrap;-webkit-box-pack:justify;justify-content:space-between;margin-bottom:10px}.story-item[data-v-210453ba]{overflow:hidden;cursor:pointer;margin-bottom:5px;flex-basis:16%}.story-item .img[data-v-210453ba]{width:100%;height:100%;-webkit-transition:all .35s ease-in-out;transition:all .35s ease-in-out;-webkit-transform:scale(1);transform:scale(1)}.story-item .img img[data-v-210453ba]{width:100%}.story-item .img[data-v-210453ba]:hover{-webkit-transform:scale(1.1);transform:scale(1.1)}@media (max-width:779px){.story-item[data-v-210453ba]{flex-basis:24.5%}}',
+        ''
+      ])
+    },
+    178: function(t, e, r) {
+      'use strict'
+      r.r(e)
+      var n = { name: 'Home', layout: 'layout' },
+        d = (r(172), r(8)),
+        component = Object(d.a)(
+          n,
+          function() {
+            var t = this.$createElement,
+              e = this._self._c || t
+            return e('div', { staticClass: 'main' }, [
+              e('div', { staticClass: 'container' }, [
+                this._m(0),
+                this._v(' '),
+                e(
+                  'div',
+                  { staticClass: 'story' },
+                  this._l(36, function(t) {
+                    return e('div', { key: t, staticClass: 'story-item' }, [
+                      e('div', { staticClass: 'img' }, [
+                        e('div', { staticClass: 'img' }, [
+                          e('img', {
+                            attrs: { src: '/images/index/a' + t + '.png' }
+                          })
+                        ])
+                      ])
+                    ])
+                  }),
+                  0
+                )
+              ])
+            ])
+          },
+          [
+            function() {
+              var t = this.$createElement,
+                e = this._self._c || t
+              return e('div', { staticClass: 'brand' }, [
+                e('div', { staticClass: 'brand-item' }, [
+                  e('div', { staticClass: 'img' }, [
+                    e('img', {
+                      attrs: { src: '/images/index/brand1.png', alt: '' }
+                    })
+                  ])
+                ]),
+                this._v(' '),
+                e('div', { staticClass: 'brand-item' }, [
+                  e('div', { staticClass: 'img' }, [
+                    e('img', {
+                      attrs: { src: '/images/index/brand2.png', alt: '' }
+                    })
+                  ])
+                ]),
+                this._v(' '),
+                e('div', { staticClass: 'brand-item' }, [
+                  e('div', { staticClass: 'img' }, [
+                    e('img', {
+                      attrs: { src: '/images/index/brand3.png', alt: '' }
+                    })
+                  ])
+                ]),
+                this._v(' '),
+                e('div', { staticClass: 'brand-item' }, [
+                  e('div', { staticClass: 'img' }, [
+                    e('img', {
+                      attrs: { src: '/images/index/brand4.png', alt: '' }
+                    })
+                  ])
+                ]),
+                this._v(' '),
+                e('div', { staticClass: 'brand-item' }, [
+                  e('div', { staticClass: 'img' }, [
+                    e('img', {
+                      attrs: { src: '/images/index/brand5.png', alt: '' }
+                    })
+                  ])
+                ]),
+                this._v(' '),
+                e('div', { staticClass: 'brand-item' }, [
+                  e('div', { staticClass: 'img' }, [
+                    e('img', {
+                      attrs: { src: '/images/index/brand6.png', alt: '' }
+                    })
+                  ])
+                ])
+              ])
+            }
+          ],
+          !1,
+          null,
+          '210453ba',
+          null
+        )
+      e.default = component.exports
+    }
+  }
+])
