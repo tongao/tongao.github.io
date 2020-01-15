ff06164860c52849098e0e2db0e6462d3eb19bbd
Ta <ao.tong@adachina.com>
2020-01-15 13:57:00 +0800

    ¿ÔÓ˝Ï‰ˇÍÎÍØÒÁ¨€ÁÁ„„„

---
+++ search/precache-manifest.49f682645cbd5c23d416a224a04c92a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33a5b9cd20636a94c15029c77351c4e2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6994f79c87473cc42e8b",
+    "url": "/static/js/2.246356cb.chunk.js"
+  },
+  {
+    "revision": "6843e0c60308b05ada4a76c1b67f6fc8",
+    "url": "/static/js/2.246356cb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3846b00a7287f4777da8",
+    "url": "/static/js/main.21e950cc.chunk.js"
+  },
+  {
+    "revision": "61c6d69357a5ccb0533d",
+    "url": "/static/js/runtime-main.90cf32b6.js"
+  },
+  {
+    "revision": "dce979257a5e184382c46cb4671f6d84",
+    "url": "/static/media/bg.dce97925.png"
+  }
+]);
